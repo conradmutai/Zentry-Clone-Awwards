853b184563bb27e38ba1e108544079f3892b43c3
conradmutai <conrad.mutai@gmail.com>
2025-03-21 21:20:06 -0400

    Hero Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Hero from "./components/hero.jsx";
+import About from "./components/about.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Features from "./components/Features.jsx";
 
 const App = () => {
     return (
         <main className={"relative min-h-screen w-screen overflow-x-hidden"}>
+            <Navbar />
             <Hero />
-
+            <About />
+            <Features />
         </main>
     )
 }
